809a976bef1e6d265b27d3590c155701806d3cb6
n0uur <noal.kt@gmail.com>
2020-11-28 03:55:31 +0700

    game init for dummy menu

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,11 @@
 public class Game {
-    public static void main(String[] args) {
-        
+    public static enum GameState {
+        MAIN_MENU,
+        GAME_PLAYING,
+        NEED_EXIT
+    }
+
+    public Game() {
+        new GameMainMenuController();
     }
 }
