2d9efc5eedf41d88929d4761a50162ad2e0299c8
n0uur <noal.kt@gmail.com>
2020-11-28 03:54:46 +0700

    finished gamemainmenu view

--- src/GameMainMenuView.java
+++ src/GameMainMenuView.java
@@ -1,3 +1,5 @@
+import Shared.Logger.GameLog;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -44,29 +46,55 @@ public class GameMainMenuView {
         this.joinPanel = new JPanel();
         this.joinPanel.setLayout(new BorderLayout());
 
+        JPanel joinTopPanel = new JPanel();
+        joinTopPanel.setLayout(new GridLayout(1, 2));
+
         this.ipTextField = new JTextField();
         this.ipLabel = new JLabel("Host's IP :");
+
+        joinTopPanel.add(this.ipLabel);
+        joinTopPanel.add(this.ipTextField);
+
+        JPanel joinBottomPanel = new JPanel();
+        joinBottomPanel.setLayout(new FlowLayout());
         this.backFromJoinButton = new JButton("Back");
         this.backFromJoinButton.addActionListener(gameMainMenuController);
         this.connectButton = new JButton("Connect");
-        this.backFromJoinButton.addActionListener(gameMainMenuController);
+        this.connectButton.addActionListener(gameMainMenuController);
 
-        this.joinPanel.add(ipLabel, BorderLayout.NORTH);
-        this.joinPanel.add(ipTextField, BorderLayout.NORTH);
-        this.joinPanel.add(backFromJoinButton);
-        this.joinPanel.add(connectButton);
+        joinBottomPanel.add(backFromJoinButton);
+        joinBottomPanel.add(connectButton);
+
+        this.joinPanel.add(joinTopPanel, BorderLayout.NORTH);
+        this.joinPanel.add(joinBottomPanel);
 
         this.mainMenuFrame.setLayout(new BorderLayout());
         this.mainMenuFrame.add(this.topPanel, BorderLayout.NORTH);
-        this.mainMenuFrame.add(this.mainPanel);
 
-        this.mainMenuFrame.pack();
+        this.updateMainMenu();
+
+        this.mainMenuFrame.setSize(400, 140);
         this.mainMenuFrame.setVisible(true);
         this.mainMenuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    public void updateMainMenu() {
+        if(this.isShowingJoinMenu) {
+            this.mainMenuFrame.remove(this.mainPanel);
+            this.mainMenuFrame.add(this.joinPanel, BorderLayout.CENTER);
+        }
+        else {
+            this.mainMenuFrame.remove(this.joinPanel);
+            this.mainMenuFrame.add(this.mainPanel, BorderLayout.CENTER);
+        }
+        this.mainMenuFrame.revalidate();
+        this.mainMenuFrame.repaint();
+    }
+
     public void setShowingJoinMenu(boolean showing) {
+        GameLog.Log("updating join game menu.. " + showing);
         this.isShowingJoinMenu = showing;
+        this.updateMainMenu();
     }
 
     public JTextField getNameTextField() {
