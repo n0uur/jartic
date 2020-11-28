500a7e82b9a9bd8166414322040dceef9719df02
n0uur <noal.kt@gmail.com>
2020-11-29 05:30:07 +0700

    add PlayerProfile dummy class

---
+++ src/Shared/CanSendAsPacket.java
@@ -0,0 +1,5 @@
+package Shared;
+
+public interface CanSendAsPacket {
+    public String toPacketString();
+}
---
+++ src/Shared/Model/PlayerProfile.java
@@ -0,0 +1,11 @@
+package Shared.Model;
+
+import Shared.CanSendAsPacket;
+
+public class PlayerProfile implements CanSendAsPacket {
+
+    @Override
+    public String toPacketString() {
+        return null;
+    }
+}
