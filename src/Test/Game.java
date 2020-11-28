89b9b8482d1b779a07d67f0b3238f28912de5cc5
n0uur <noal.kt@gmail.com>
2020-11-29 06:47:11 +0700

    Add GSON test about enum.. :love_letter:

--- src/Test/Game.java
+++ src/Test/Game.java
@@ -3,6 +3,14 @@ package Test;
 import java.util.ArrayList;
 
 public class Game {
+
+    public static enum j {
+        AAAA,
+        BBBB,
+        CCCC
+    }
+
     public String name;
+    public j enn;
     public ArrayList<Video> intro;
 }
--- src/Test/JsonParseTesting.java
+++ src/Test/JsonParseTesting.java
@@ -6,11 +6,13 @@ import com.google.gson.Gson;
 import java.util.ArrayList;
 
 public class JsonParseTesting {
+
     public static void main(String[] args) {
         Gson gson = new Gson();
 
         Game game = new Game();
         game.name = "The Sims";
+        game.enn = Game.j.BBBB;
         game.intro = new ArrayList<Video>();
         game.intro.add(new Video("asdasd"));
         game.intro.add(new Video("fasf"));
@@ -26,5 +28,7 @@ public class JsonParseTesting {
 
         System.out.println(reversed.name);
         System.out.println(reversed.intro.get(1).name);
+        System.out.println(reversed.enn == Game.j.BBBB);
+        System.out.println(reversed.enn == Game.j.AAAA);
     }
 }
