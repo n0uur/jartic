a615756fc0d1aac5e9a985dafb3d79db206f584e
FuMiRiO <50404762+FuMiRiO@users.noreply.github.com>
2020-11-28 23:37:12 +0700

    set Look and Feel

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        new Game();
+//        new Game();
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        SwingUtilities.invokeLater(() -> {new Game();});
     }
 }
