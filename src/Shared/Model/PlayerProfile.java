f229da19db31cdf162f8d68485988a37d4976947
n0uur <noal.kt@gmail.com>
2020-11-30 03:07:41 +0700

    :+1: Simple design for PlayerProfile

--- src/Shared/Model/PlayerProfile.java
+++ src/Shared/Model/PlayerProfile.java
@@ -2,4 +2,39 @@ package Shared.Model;
 
 public class PlayerProfile {
 
+    private String name;
+    private int score;
+
+    public PlayerProfile() {
+        this("");
+    }
+
+    public PlayerProfile(String name) {
+        this.setName(name);
+        this.setScore(0);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void addScore(int score) {
+        this.score += score;
+    }
+
+    public void resetScore() {
+        this.score = 0;
+    }
 }
