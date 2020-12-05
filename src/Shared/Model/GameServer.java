ab60af8b4114fa4c137944d28ef0db6275ad1c61
n0uur <noal.kt@gmail.com>
2020-12-05 23:50:47 +0700

    server game status with comment todo

--- src/Shared/Model/GameServer.java
+++ src/Shared/Model/GameServer.java
@@ -2,11 +2,13 @@ package Shared.Model;
 
 public class GameServer {
     public static enum gameStatus {
-        GAME_WAITING,
-        GAME_STARTING,
-        GAME_PLAYING,
-        GAME_WAITING_WORD,
-        GAME_ENDED_ROUND,
-        GAME_ENDED,
+        GAME_WAITING, // รอผู้เล่นมา
+        GAME_STARTING, // เกมกำลังเริ่ม
+        GAME_STARTING_ROUND, // นำ player ที่กำลังอยู่ในเกมใส่ array เป็นคิวที่ต้องเล่น
+        GAME_WAITING_WORD, // รอผู้เล่นที่เป็นตาวาดเลือกคำ
+        GAME_PLAYING, // กำลังเล่น วาด หรือเดา
+        GAME_NEXT_PLAYER, // กำลังรอเพื่อเปลี่ยนผู้เล่นเป็นคนต่อไป แสดงผลคำตอบ
+        GAME_ENDED_ROUND, // ผูเล่นทุกคนในรอบ วาดหมดแล้ว เริ่มรอบต่อไปถ้าไม่มี คะแนน > 120 หรือจบเกม
+        GAME_ENDED, // จบเกม เครียคะแนน ผู้เล่น ประกาศผลผู้ชนะ แล้วเตรียมเริ่มเกมใหม่
     }
 }
