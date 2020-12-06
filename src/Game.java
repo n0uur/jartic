84b494d2bc2af67bd79b4a1ce30a489fa1ee215f
n0uur <noal.kt@gmail.com>
2020-12-06 15:50:31 +0700

    :video_game: now game running on individual thread

--- src/Game.java
+++ src/Game.java
@@ -2,7 +2,9 @@ import GameClient.GameClient;
 import GameClient.Model.GameClientStatus;
 import GameServer.GameServer;
 
-public class Game {
+import javax.swing.*;
+
+public class Game implements Runnable {
     public static enum GameState {
         MAIN_MENU,
         GAME_PLAYING,
@@ -14,39 +16,20 @@ public class Game {
     private GameState gameState;
 
     public Game() {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         new GameMainMenu(this);
 
         this.gameState = GameState.MAIN_MENU;
     }
 
     public void main_loop() {
-        while (gameState != GameState.NEED_EXIT) {
 
-            if(this.gameServer != null) {
-                this.gameServer.update();
-            }
-
-            if(this.gameClient != null) {
-                this.gameClient.update();
-
-                if(this.gameClient.getGameStatus() == GameClientStatus.GAME_NEED_TO_QUIT) {
-                    this.gameState = GameState.NEED_EXIT;
-                }
-            }
-
-            try {
-                Thread.sleep(30); // around 30 FPS
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        if(this.gameServer != null) {
-            // todo : told all client, server is closing...
-            this.gameServer.destroy();
-        }
-
-        System.exit(0);
     }
 
     public void startGameServer() {
@@ -55,5 +38,10 @@ public class Game {
 
     public void startGameClient() {
         this.gameClient = new GameClient();
+    }
+
+    @Override
+    public void run() {
+        this.main_loop();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,8 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         Game game = new Game();
-        game.main_loop();
+        Thread gameThread = new Thread(game);
+        gameThread.start();
     }
 }
