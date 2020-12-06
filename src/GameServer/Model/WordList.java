57675f0045de208ae65c5e2d54aa204026a8520b
n0uur <noal.kt@gmail.com>
2020-12-06 17:05:22 +0700

    :cloud: load word list from file

---
+++ src/GameServer/Model/WordList.java
@@ -0,0 +1,56 @@
+package GameServer.Model;
+
+import Shared.Logger.GameLog;
+import Shared.Logger.ServerLog;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class WordList {
+
+    private static final String saveFileName = "words.txt";
+
+    private static ArrayList<String> wordList;
+
+    public static ArrayList<String> getWordList() {
+        return wordList;
+    }
+
+    public static String[] getRandomWord(int size) {
+        ArrayList<String> shuffleList = new ArrayList<>(wordList);
+        Collections.shuffle(shuffleList);
+
+        String[] randomWord = new String[size];
+
+        for(int i = 0 ; i < size; i++) {
+            randomWord[i] = shuffleList.get(i);
+        }
+
+        return randomWord;
+
+    }
+
+    public static void loadWordList() {
+        File wordListFile = new File(saveFileName);
+        if(wordListFile.exists()) {
+            wordList = new ArrayList<String>();
+            try (BufferedReader reader = new BufferedReader(new FileReader(wordListFile))) {
+                String line = reader.readLine();
+                int wordsCount = 0;
+                while (line != null) {
+                    wordList.add(line);
+                    wordsCount++;
+                    line = reader.readLine();
+                }
+                ServerLog.Log("loaded " + wordsCount + " words from file..");
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            ServerLog.error("failed to read words list.. please add word and save to " + saveFileName + " file.");
+        }
+    }
+}
