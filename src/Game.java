d106376b79388807c8b727a11b22525a74b21536
n0uur <noal.kt@gmail.com>
2020-12-06 17:06:04 +0700

    :video_game: change game into individual thread

--- src/Game.java
+++ src/Game.java
@@ -26,10 +26,38 @@ public class Game implements Runnable {
         new GameMainMenu(this);
 
         this.gameState = GameState.MAIN_MENU;
+
+        this.main_loop();
     }
 
     public void main_loop() {
+        while (gameState != GameState.NEED_EXIT) {
 
+            if(this.gameServer != null) {
+                this.gameServer.update();
+            }
+
+            if(this.gameClient != null) {
+                this.gameClient.update();
+
+                if(this.gameClient.getGameStatus() == GameClientStatus.GAME_NEED_TO_QUIT) {
+                    this.gameState = GameState.NEED_EXIT;
+                }
+            }
+
+            try {
+                Thread.sleep(30); // around 30 FPS
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(this.gameServer != null) {
+            // todo : told all client, server is closing...
+            this.gameServer.destroy();
+        }
+
+        System.exit(0);
     }
 
     public void startGameServer() {
