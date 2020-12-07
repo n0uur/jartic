887f8046ceedf372fd134a66b548faa7f5193cb0
n0uur <noal.kt@gmail.com>
2020-12-07 19:39:14 +0700

    :video_game: lazy update player method

--- src/GameClient/Model/ClientPlayer.java
+++ src/GameClient/Model/ClientPlayer.java
@@ -1,5 +1,6 @@
 package GameClient.Model;
 
+import Shared.Logger.GameLog;
 import Shared.Model.PlayerProfile;
 
 import java.util.ArrayList;
@@ -31,33 +32,52 @@ public class ClientPlayer {
 
     public synchronized static void updatePlayers(ArrayList<PlayerProfile> newPlayers) {
 
+        players = new ArrayList<>();
         for(int i = 0; i < newPlayers.size(); i++) {
             PlayerProfile newPlayer = newPlayers.get(i);
 
-            ClientPlayer localPlayer = ClientPlayer.getPlayer(newPlayer.getId());
-            if(localPlayer != null) { // have any player in local array
-                localPlayer.playerProfile.setScore(newPlayer.getScore());
-                localPlayer.playerProfile.isDrawing(newPlayer.isDrawing());
-            }
-            else { // new player appeared!
-                new ClientPlayer(newPlayer);
-            }
+            new ClientPlayer(newPlayer);
+            GameLog.log("updated player : " + newPlayer.getName() + " (" + newPlayer.getId() + ")");
         }
 
-        // check for player that removed from server..
-        Iterator<ClientPlayer> playersIterator = players.iterator();
 
-        while (playersIterator.hasNext()) {
-            ClientPlayer player = playersIterator.next();
-
-            for(int j = 0; j < newPlayers.size(); j++) {
-                if(player.getPlayerProfile().getId() == newPlayers.get(j).getId()) {
-                    break;
-                }
-                // player not found maybe his/her disconnected, just remove..
-                playersIterator.remove();
-            }
-        }
+//        for(int i = 0; i < newPlayers.size(); i++) {
+//            PlayerProfile newPlayer = newPlayers.get(i);
+//
+//            ClientPlayer localPlayer = ClientPlayer.getPlayer(newPlayer.getId());
+//            if(localPlayer != null) { // have any player in local array
+//                localPlayer.playerProfile.setScore(newPlayer.getScore());
+//                localPlayer.playerProfile.isDrawing(newPlayer.isDrawing());
+//
+//                GameLog.log("Updated player : " + newPlayer.getName());
+//            }
+//            else { // new player appeared!
+//                new ClientPlayer(newPlayer);
+//                GameLog.log("New player in coming : " + newPlayer.getName() + " (" + newPlayer.getId() + ")");
+//            }
+//        }
+//
+//        try {
+//            Thread.sleep(10);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // check for player that removed from server..
+//        Iterator<ClientPlayer> playersIterator = getPlayers().iterator();
+//
+//        while (playersIterator.hasNext()) {
+//            ClientPlayer player = playersIterator.next();
+//
+//            for(int j = 0; j < newPlayers.size(); j++) {
+//                if(player.getPlayerProfile().getId() == newPlayers.get(j).getId()) {
+//                    break;
+//                }
+//                // player not found maybe his/her disconnected, just remove..
+//                GameLog.log("removed " + player.getPlayerProfile().getName() + " ("+ player.getPlayerProfile().getId() +") bye :(");
+//                playersIterator.remove();
+//            }
+//        }
     }
 
     ////////////////////////////////////////
