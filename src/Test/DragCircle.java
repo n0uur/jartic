b7991a4b4a18d8a6762686ae4adfd3de5a6dbffe
n0uur <noal.kt@gmail.com>
2020-12-07 15:26:00 +0700

    :video_game: example for drawing, no glitch, no lost data

--- src/Test/DragCircle.java
+++ src/Test/DragCircle.java
@@ -15,6 +15,8 @@ import javax.swing.text.StyleContext;
 
 class DragCircle extends JPanel {
 
+    protected int[][] points = new int[800][400];
+
     private final class MouseDrag extends MouseAdapter {
         private boolean dragging = false;
         private Point last;
@@ -22,13 +24,13 @@ class DragCircle extends JPanel {
         @Override
         public void mousePressed(MouseEvent m) {
             last = m.getPoint();
-            dragging = isInsideEllipse(last);
-            if (!dragging) {
-                x = last.x;
-                y = last.y;
-                width = 0;
-                height = 0;
-            }
+            dragging = true;
+//            if (!dragging) {
+//                x = last.x;
+//                y = last.y;
+//                width = 0;
+//                height = 0;
+//            }
             repaint();
         }
 
@@ -41,16 +43,19 @@ class DragCircle extends JPanel {
 
         @Override
         public void mouseDragged(MouseEvent m) {
-            int dx = m.getX() - last.x;
-            int dy = m.getY() - last.y;
-            if (dragging) {
-                x += dx;
-                y += dy;
-            } else {
-                width += dx;
-                height += dy;
+            if(dragging) {
+                points[m.getX()][m.getY()] = 1;
             }
-            last = m.getPoint();
+//            int dx = m.getX() - last.x;
+//            int dy = m.getY() - last.y;
+//            if (dragging) {
+//                x += dx;
+//                y += dy;
+//            } else {
+//                width += dx;
+//                height += dy;
+//            }
+//            last = m.getPoint();
             repaint();
         }
     }
@@ -76,26 +81,32 @@ class DragCircle extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawOval(x, y, width, height);
+//        g.drawOval(x, y, width, height);
+        for(int i = 0; i < 800; i++) {
+            for(int j = 0; j < 400; j++) {
+                if(points[i][j] == 1)
+                g.fillOval(i, j, 10, 10);
+            }
+        }
     }
 
     public static void main(String[] args) {
         JFrame jFrame = new JFrame();
-        jFrame.setSize(300, 300);
-//        jFrame.add(new DragCircle());
-        JTextPane pane = new JTextPane();
-        jFrame.add(pane);
+        jFrame.setSize(800, 400);
+        jFrame.add(new DragCircle());
+//        JTextPane pane = new JTextPane();
+//        jFrame.add(pane);
         jFrame.setVisible(true);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        for(int i = 0; i < 100; i++) {
-            DragCircle.appendToPane(pane, i + "\n", Color.ORANGE);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+//        for(int i = 0; i < 100; i++) {
+//            DragCircle.appendToPane(pane, i + "\n", Color.ORANGE);
+//            try {
+//                Thread.sleep(100);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
     public static void appendToPane(JTextPane tp, String msg, Color c)
